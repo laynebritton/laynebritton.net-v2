a7754c078600516222be1b73a4694c1dfc428316
laynebritton <laynebritton1998@gmail.com>
2022-11-16 16:23:48 -0500

    Extending functionality of Responsive Content to the three common use cases

--- src/components/ResponsiveContent/ResponsiveContent.tsx
+++ src/components/ResponsiveContent/ResponsiveContent.tsx
@@ -1,19 +1,55 @@
 import React from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
 
-type ResponsiveContentProps = {
+export enum ResponsiveContentType {
+  CONTAINER,
+  ROW,
+  COLUMN
+}
+
+export type ResponsiveContentProps = {
   children: React.ReactNode;
+  type?: ResponsiveContentType;
 };
 
-export const ResponsiveContent = ({ children }: ResponsiveContentProps) => {
-  return (
-    <Container>
-      <Row>
-        <Col xs="1" className={'d-xs-block d-sm-none'}></Col>
-        <Col xs="10" sm="8">
-          {children}
-        </Col>
-      </Row>
-    </Container>
-  );
+/* 
+Shorthand component for the commonly re-used page margins
+*/
+export const ResponsiveContent = ({
+  children,
+  type = ResponsiveContentType.CONTAINER
+}: ResponsiveContentProps) => {
+  switch (type) {
+    case ResponsiveContentType.CONTAINER:
+      return (
+        <Container>
+          <Row>
+            <Col xs="1" className={'d-xs-block d-sm-none'}></Col>
+            <Col xs="10" sm="8">
+              {children}
+            </Col>
+          </Row>
+        </Container>
+      );
+
+    case ResponsiveContentType.ROW:
+      return (
+        <Row>
+          <Col xs="1" className={'d-xs-block d-sm-none'}></Col>
+          <Col xs="10" sm="8">
+            {children}
+          </Col>
+        </Row>
+      );
+
+    case ResponsiveContentType.COLUMN:
+      return (
+        <>
+          <Col xs="1" className={'d-xs-block d-sm-none'}></Col>
+          <Col xs="10" sm="8">
+            {children}
+          </Col>
+        </>
+      );
+  }
 };
