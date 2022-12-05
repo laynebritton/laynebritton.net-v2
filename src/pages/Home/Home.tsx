748ee47d1f74848ee6d5cf97a72f736707ec6653
laynebritton <laynebritton1998@gmail.com>
2022-12-05 15:07:11 -0500

    MVP for home image background opacque delayed entry

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,4 +1,5 @@
 import { FC } from 'react';
+import { motion } from 'framer-motion';
 import Hero from '../../components/Hero/Hero';
 import styles from './Home.module.css';
 import { TrackEvent } from '../../util/Analytics';
@@ -9,9 +10,39 @@ interface HomeProps {}
 const Home: FC<HomeProps> = () => {
   TrackEvent(ANALYTICS_CONSTANTS.VIEW_HOME_PAGE);
   return (
-    <div className={styles.Home} data-testid="Home">
-      <Hero />
-    </div>
+    <>
+      <div
+        className={'d-md-block d-sm-none'}
+        style={{
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          zIndex: -1,
+          height: '100%',
+          width: '100%',
+          overflow: 'hidden',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center'
+        }}
+      >
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 0.3 }}
+          transition={{ duration: 3.0, delay: 1.5 }}
+        >
+          <img
+            style={{ objectFit: 'cover' }}
+            // src="https://s3.amazonaws.com/laynebritton.net-media/public/cold_spring_big_dipper_lower_2_png.png"
+            src="https://s3.amazonaws.com/laynebritton.net-media/public/cold_spring_starry_sky_png_lower.png"
+            // src="https://s3.amazonaws.com/laynebritton.net-media/public/rosebud_trail_lowered_png.png"
+          ></img>
+        </motion.div>
+      </div>
+      <div className={styles.Home} data-testid="Home">
+        <Hero />
+      </div>
+    </>
   );
 };
 
